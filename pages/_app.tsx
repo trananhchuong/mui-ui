2625d411cfa026c49d2eac88299c85035484fa74
chuong tran <chuongninolxag@gmail.com>
2023-01-13 15:43:54 +0700

    config prettierrc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline, StyledEngineProvider } from "@mui/material";
-import type { AppProps } from "next/app";
-import themes from "../themes/index";
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import themes from '../themes/index';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
