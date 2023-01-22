62856d92ae487ebf1db01c35c128671db2938981
chuong tran <chuongninolxag@gmail.com>
2023-01-22 21:20:03 +0700

    add seo data & footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common';
 import { FeatureWorks, HeroSection, RecentPosts } from '@/components/home';
 import { MainLayout } from '@/components/layout';
 import { Box } from '@mui/material';
@@ -12,6 +13,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Box>
+        <Seo
+          data={{
+            title: 'NextJS Tutorials | Easy Frontend',
+            description:
+              'Step by step tutorials to build a full CRUD website using NextJS for beginners',
+            url: 'https://learn-nextjs-fawn.vercel.app/',
+            thumbnailUrl:
+              'https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs.png',
+          }}
+        />
         <HeroSection />
         <RecentPosts />
         <FeatureWorks />
